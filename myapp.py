e1342141c4732f80aa6396d2be78f119330203c6
bmukum <bmukum99@gmail.com>
2020-07-23 14:59:51 -0400

    added the app

---
+++ myapp.py
@@ -0,0 +1,36 @@
+print("Welcome to My Application!!!")
+print("In this application, you will THIS PROGRAMchoose a random number, and I will use it to perform some operations. Then,I will tell you the final answer!!")
+
+print("You will need a calculator to perform the operations. So use a pen and paper, or your phone's calculator!")
+
+print("Are you ready? Let's get started!")
+
+print("Think of  any number,X,  and make sure you remember it. You will use it for the next operation.")
+
+print(" You have not told me the value of X, but I will use it to perform an exercise and give you the final answer!")
+
+print("Now, enter yes after carrying out each operation below")
+
+print("Remember to take note of the result of each step, as it is needed for the next.")
+
+confirm =  str ( input ("Have you thought of the number, X ? "))
+
+sum = str ( input ("Add the number to itself, and take the note of the sum? "))
+
+multiplication = str ( input ("Multiply the sum by 8?:"))
+
+divide_by_x = str ( input ("Divide your result by X?a "))
+
+add = str (  input ("Now add 4 to the number ?"))
+
+print("LAST STEP:")
+
+final= str (input ("Now divide your answer by two?"))
+
+get= str(input("Good! Press enter to see your final answer"))
+
+
+print("The final answer is 10!!!")
+
+
+
